3c1771d5973b14834e8cdda58a517d353143fda6
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2024-05-14 16:04:23 -0700

    Add more default options

--- R/0_setup.R
+++ R/0_setup.R
@@ -4,18 +4,24 @@ rm(list=ls())
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(purrr)
 
 # Shiny Options ================================================================
 options(shiny.fullstacktrace = TRUE)
-do.debug = T
 
 ## Create default data =========================================================
 default = list()
 
 # Default values for study setup
-default$study$n_intervenions = NA_integer_
-default$study$n_groups = NA_integer_
+default$study$n_INT = NA_integer_
+default$study$n_COH = NA_integer_
 default$study$time_units = ""
-default$study$group_units = "Group"
+default$study$COH_units = "Group"
+default$study$INT_length = NA_integer_
+default$study$INT_start_max = Inf
+default$study$INT_end_max = Inf
+default$study$INT_gap = 0
+default$study$INT_null_offset = NA # When offset is uninitialized, prevents phantom study from being generated
+default$study$INT_offset = 1
 
 # Default values for visualization setup

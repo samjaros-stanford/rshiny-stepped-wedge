701349fbb37563c9a04f789eaa80cf8cf7172392
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2024-06-13 23:54:29 -0700

    Fix issues with generating study based on config
    **Remaining Issue:**
    Uses a for-loop because each start time depends on the previous end time, which must be calculated at the same time. I was unable to figure out how to do this with funcitonal programming

--- R/6_generate_study.R
+++ R/6_generate_study.R
@@ -26,9 +26,9 @@ generate_study <- function(base_study){
     group_by(COH) %>%
     filter(row_number() == 1) %>%
     ungroup() %>%
-    mutate(INT_end = INT_gap + INT_length + cumsum(INT_offset),
+    mutate(INT_end = INT_gap + INT_length + (cumsum(INT_offset) - 1),
            INT_start = if_else(INT_end - INT_start_max < 0,
-                               0 + INT_gap,
+                               INT_gap,
                                INT_end - INT_start_max + INT_gap)) %>%
     select(COH, INT, INT_length, INT_gap, INT_start, INT_end) %>%
     # Calculate middle interventions
@@ -37,14 +37,22 @@ generate_study <- function(base_study){
                 group_by(COH) %>%
                 filter(row_number()!=1 & row_number()!=n()) %>%
                 ungroup()) %>%
-    arrange(COH, INT) %>%
-    mutate(INT_start = if_else(is.na(INT_start),
-                                  lag(INT_end) + INT_gap,
-                                  INT_start),
-           INT_end = if_else(is.na(INT_end),
-                                INT_start + INT_length,
-                                INT_end)) %>%
-    select(COH, INT, INT_start, INT_end)
+    arrange(COH, INT)
+  
+  # How to do this without for loop???
+  for(i in 1:nrow(not_last_INT)){
+    # If INT_start has already been figured out, we're good
+    # This also takes care of when i=1
+    if(!is.na(not_last_INT[i, "INT_start"])){
+      next
+    }
+
+    not_last_INT[i, "INT_start"] =
+      not_last_INT[i-1, "INT_end"] + not_last_INT[i, "INT_gap"]
+
+    not_last_INT[i, "INT_end"] =
+      not_last_INT[i, "INT_start"] + not_last_INT[i, "INT_length"]
+  }
   
   last_INT <- base_study %>%
     arrange(COH, INT) %>%
@@ -63,25 +71,29 @@ generate_study <- function(base_study){
     mutate(study_end = max(prev_INT_end + INT_gap + INT_length)) %>%
     # Get start and end times where the end time is the smaller of study_end or 
     #   the max length of the intervention
+    rowwise() %>%
     mutate(INT_start = INT_gap + prev_INT_end,
            INT_end = suppressWarnings(
-             min(study_end, INT_start + INT_length + INT_end_max))) %>%
+             min(study_end, INT_start + INT_end_max))) %>%
+    ungroup() %>%
     select(COH, INT, INT_start, INT_end)
 
-  rbind(not_last_INT, last_INT) %>%
+  rbind(not_last_INT %>%
+          select(COH, INT, INT_start, INT_end), 
+        last_INT) %>%
     arrange(COH, INT)
 }
 
 #===============================================================================
 # Testing
 
-# base_study <- data.frame(COH = 1:4) %>%
-#   cross_join(data.frame(INT = 1:3,
-#                         INT_length = c(1,2,3),
-#                         INT_offset = 1,
-#                         INT_gap = c(0,0,0),
-#                         INT_start_max = 1,
-#                         INT_end_max = 3))
+# base_study <- data.frame(COH = 1:3) %>%
+#   cross_join(data.frame(INT = 1:5,
+#                         INT_length = 1,
+#                         INT_offset = 2,
+#                         INT_gap = 0,
+#                         INT_start_max = 2,
+#                         INT_end_max = 2))
 # 
 # generate_study(base_study=base_study)
   

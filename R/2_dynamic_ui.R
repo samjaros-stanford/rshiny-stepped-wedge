05a89c3fe7ec9de2eaa35685854852dc00076040
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2024-08-02 16:28:37 -0700

    Fix issues with too many interventions showing up for <3 interventions (#10) Add function for drawing intervention inputs in cohort tabs

--- R/2_dynamic_ui.R
+++ R/2_dynamic_ui.R
@@ -14,14 +14,13 @@ make_INT_name_ui <- function(input){
              label = paste0("Name of intervention ", i),
              value = ifelse(is.null(input[[paste0("INT_name_", i)]]),
                             "",
-                            input[[paste0("INT_name_", i)]]))
-         })
+                            input[[paste0("INT_name_", i)]]))})
 }
 
-# Create intervention timing tab -----------------------------------------------
-# Combine existing input widgets with their names from input reactive
+# Create intervention timing tabs -----------------------------------------------
+# Create input widgets with their names from input reactive
 make_INT_timing_ui <- function(input){
-  c(list(
+  INT_timing_ui <- list(
     # Heading for first intervention
     tags$u(strong(ifelse(input$INT_name_1 == "",
                          paste0("Intervention 1"),
@@ -34,86 +33,188 @@ make_INT_timing_ui <- function(input){
                label = "Delay Before",
                value = ifelse(is.null(input$INT_gap_1),
                               default$study$INT_gap,
-                              input$INT_gap_1)
-             )),
+                              input$INT_gap_1))),
       column(width = 4,
              numericInput(
                inputId = "INT_length_1",
                label = "Min Duration",
                value = ifelse(is.null(input$INT_length_1),
                               default$study$INT_length,
-                              input$INT_length_1)
-             )),
+                              input$INT_length_1))),
       column(width = 4,
              numericInput(
                inputId = "INT_start_max",
                label = "Max Duration",
                value = ifelse(is.null(input$INT_start_max),
                               default$study$INT_start_max,
-                              input$INT_start_max)
-             ))
-    )),
-    sapply(
-      2:(input$n_INT - 1),
-      function(i){
-        list(
-          tags$u(strong(ifelse(input[[paste0("INT_name_", i)]] == "",
-                               paste0("Intervention ", i),
-                               input[[paste0("INT_name_", i)]]))),
-          fluidRow(
-            column(
-              width = 6,
-              numericInput(
-                inputId = paste0("INT_gap_", i),
-                label = "Delay Before",
-                value = ifelse(is.null(input[[paste0("INT_gap_", i)]]),
-                               default$study$INT_gap,
-                               input[[paste0("INT_gap_", i)]])
-              )),
-            column(
-              width = 6,
-              numericInput(
-                inputId = paste0("INT_length_", i),
-                label = "Duration",
-                value = ifelse(is.null(input[[paste0("INT_length_", i)]]),
-                               default$study$INT_length,
-                               input[[paste0("INT_length_", i)]])
-              ))
-          )
-        )
-      }),
-    list(tags$u(strong(ifelse(input[[paste0("INT_name_", input$n_INT)]] == "",
-                              paste0("Intervention ", input$n_INT),
-                              input[[paste0("INT_name_", input$n_INT)]]))),
-         fluidRow(
-           column(
-             width = 4,
+                              input$INT_start_max)))))
+  
+  # If there's more than two interventions, we need to add "middle"
+  #   interventions that have a definite length
+  if(input$n_INT>2){
+    INT_timing_ui <- c(
+      INT_timing_ui,
+      sapply(
+        2:(input$n_INT - 1),
+        function(i){
+          list(
+            tags$u(strong(ifelse(input[[paste0("INT_name_", i)]] == "",
+                                 paste0("Intervention ", i),
+                                 input[[paste0("INT_name_", i)]]))),
+            fluidRow(
+              column(
+                width = 6,
+                numericInput(
+                  inputId = paste0("INT_gap_", i),
+                  label = "Delay Before",
+                  value = ifelse(is.null(input[[paste0("INT_gap_", i)]]),
+                                 default$study$INT_gap,
+                                 input[[paste0("INT_gap_", i)]])
+                )),
+              column(
+                width = 6,
+                numericInput(
+                  inputId = paste0("INT_length_", i),
+                  label = "Duration",
+                  value = ifelse(is.null(input[[paste0("INT_length_", i)]]),
+                                 default$study$INT_length,
+                                 input[[paste0("INT_length_", i)]])))))}))}
+  
+  # If there's only 1 intervention, it was already added by the first step
+  if(input$n_INT>1){
+    INT_timing_ui <- c(
+      INT_timing_ui,
+      list(tags$u(strong(ifelse(input[[paste0("INT_name_", input$n_INT)]] == "",
+                                paste0("Intervention ", input$n_INT),
+                                input[[paste0("INT_name_", input$n_INT)]]))),
+           fluidRow(
+             column(
+               width = 4,
+               numericInput(
+                 inputId = paste0("INT_gap_", input$n_INT),
+                 label = "Delay Before",
+                 value = ifelse(is.null(input[[paste0("INT_gap_", input$n_INT)]]),
+                                default$study$INT_gap,
+                                input[[paste0("INT_gap_", input$n_INT)]]))),
+             column(
+               width = 4,
+               numericInput(
+                 inputId = paste0("INT_length_", input$n_INT),
+                 label = "Min Duration",
+                 value = ifelse(is.null(input[[paste0("INT_length_", input$n_INT)]]),
+                                default$study$INT_length,
+                                input[[paste0("INT_length_", input$n_INT)]]))),
+             column(
+               width = 4,
+               numericInput(
+                 inputId = "INT_end_max",
+                 label = "Max Duration",
+                 value = ifelse(is.null(input$INT_end_max),
+                                default$study$INT_end_max,
+                                input$INT_end_max))))))}
+  
+  return(INT_timing_ui)
+}
+
+# Create input widgets for custom timing by cohort from input reactive
+#   Default values should be NA/blank which will be handled by later functions
+make_INT_timing_by_COH_ui <- function(COH_id, input){
+  # NOTE: Goal is to eventually use semanticUI, which can handle numeric placeholders
+  #       however, this will be implemented in a future change
+  
+  INT_timing_ui <- list(
+    # Heading for first intervention
+    tags$u(strong(ifelse(input$INT_name_1 == "",
+                         paste0("Intervention 1"),
+                         input$INT_name_1))),
+    # First intervention specs require gap, minimum, and maximum
+    fluidRow(
+      column(width = 4,
              numericInput(
-               inputId = paste0("INT_gap_", input$n_INT),
+               inputId = paste0("INT_gap_1_COH_", COH_id),
                label = "Delay Before",
-               value = ifelse(is.null(input[[paste0("INT_gap_", input$n_INT)]]),
-                              default$study$INT_gap,
-                              input[[paste0("INT_gap_", input$n_INT)]])
-             )),
-           column(
-             width = 4,
+               value = ifelse(is.na(input[[paste0("INT_gap_1_COH_", COH_id)]]),
+                              NA,
+                              input[[paste0("INT_gap_1_COH_", COH_id)]]))),
+      column(width = 4,
              numericInput(
-               inputId = paste0("INT_length_", input$n_INT),
+               inputId = paste0("INT_length_1_COH_", COH_id),
                label = "Min Duration",
-               value = ifelse(is.null(input[[paste0("INT_length_", input$n_INT)]]),
-                              default$study$INT_length,
-                              input[[paste0("INT_length_", input$n_INT)]])
-             )),
-           column(
-             width = 4,
+               value = ifelse(is.na(input[[paste0("INT_length_1_COH_", COH_id)]]),
+                              NA,
+                              input[[paste0("INT_length_1_COH_", COH_id)]]))),
+      column(width = 4,
              numericInput(
-               inputId = "INT_end_max",
+               inputId = paste0("INT_start_max_COH_", COH_id),
                label = "Max Duration",
-               value = ifelse(is.null(input$INT_end_max),
-                              default$study$INT_end_max,
-                              input$INT_end_max)))
-           ))
-    )
+               value = ifelse(is.na(input[[paste0("INT_start_max_COH_", COH_id)]]),
+                              NA,
+                              input[[paste0("INT_start_max_COH_", COH_id)]])))))
+  
+  # If there's more than two interventions, we need to add "middle"
+  #   interventions that have a definite length
+  if(input$n_INT>2){
+    INT_timing_ui <- c(
+      INT_timing_ui,
+      sapply(
+        2:(input$n_INT - 1),
+        function(i){
+          list(
+            tags$u(strong(ifelse(input[[paste0("INT_name_", i)]] == "",
+                                 paste0("Intervention ", i),
+                                 input[[paste0("INT_name_", i)]]))),
+            fluidRow(
+              column(
+                width = 6,
+                numericInput(
+                  inputId = paste0("INT_gap_", i, "_COH_", COH_id),
+                  label = "Delay Before",
+                  value = ifelse(is.na(input[[paste0("INT_gap_", i, "_COH_", COH_id)]]),
+                                 NA,
+                                 input[[paste0("INT_gap_", i, "_COH_", COH_id)]]))),
+              column(
+                width = 6,
+                numericInput(
+                  inputId = paste0("INT_length_", i, "_COH_", COH_id),
+                  label = "Duration",
+                  value = ifelse(is.na(input[[paste0("INT_length_", i, "_COH_", COH_id)]]),
+                                 NA,
+                                 input[[paste0("INT_length_", i, "_COH_", COH_id)]])))))}))}
+  
+  # If there's only 1 intervention, it was already added by the first step
+  if(input$n_INT>1){
+    INT_timing_ui <- c(
+      INT_timing_ui,
+      list(tags$u(strong(ifelse(input[[paste0("INT_name_", input$n_INT)]] == "",
+                                paste0("Intervention ", input$n_INT),
+                                input[[paste0("INT_name_", input$n_INT)]]))),
+           fluidRow(
+             column(
+               width = 4,
+               numericInput(
+                 inputId = paste0("INT_gap_", input$n_INT, "_COH_", COH_id),
+                 label = "Delay Before",
+                 value = ifelse(is.na(input[[paste0("INT_gap_", input$n_INT, "_COH_", COH_id)]]),
+                                NA,
+                                input[[paste0("INT_gap_", input$n_INT, "_COH_", COH_id)]]))),
+             column(
+               width = 4,
+               numericInput(
+                 inputId = paste0("INT_length_", input$n_INT, "_COH_", COH_id),
+                 label = "Min Duration",
+                 value = ifelse(is.na(input[[paste0("INT_length_", input$n_INT, "_COH_", COH_id)]]),
+                                NA,
+                                input[[paste0("INT_length_", input$n_INT, "_COH_", COH_id)]]))),
+             column(
+               width = 4,
+               numericInput(
+                 inputId = paste0("INT_end_max_COH_", COH_id),
+                 label = "Max Duration",
+                 value = ifelse(is.na(input[[paste0("INT_end_max_COH_", COH_id)]]),
+                                NA,
+                                input[[paste0("INT_end_max_COH_", COH_id)]]))))))}
+  
+  return(INT_timing_ui)
 }
 
 # Create cohort customization tabs ---------------------------------------------
@@ -151,15 +252,16 @@ make_COH_customization_tab_ui <- function(COH_id, input){
       group_name = paste0("COH_bucket_group_", COH_id),
       orientation = "vertical",
       add_rank_list(
-        text = "Included intervetions",
+        text = NULL,
         labels = included_INT,
         input_id = paste0("COH_INT_incl_order_", COH_id)
       ),
       add_rank_list(
-        text = "Excluded interventions",
+        text = "Excluded from this cohort",
         labels = excluded_INT,
         input_id = paste0("COH_INT_excl_order_", COH_id)
       )
-    )
+    ),
+    make_INT_timing_by_COH_ui(COH_id, input)
   )
 }
